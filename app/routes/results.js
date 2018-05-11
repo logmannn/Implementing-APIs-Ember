1cf14a37efb0cfac6d2a9ca7cb463e6b84d782a1
lt <logantanous@gmail.com>
2018-05-11 15:53:18 -0700

    trying a districts api instead

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model: function(params) {
-    var url = 'https://whoismyrepresentative.com/getall_mems.php?output=json&zip=' + params.zip;
+    var url = 'https://cicero.azavea.com/v3.1/legislative_district?search_postal='+params.zip+'&search_country=US&format=json&key=a8f71a4e80a2e2d78d379ed3e29135da60bd2f15';
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
